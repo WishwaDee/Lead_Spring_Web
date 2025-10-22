e3bd515574f67e4d15c17396992e9b6cd5387134
Wishwa Dilshan <wishwadilshan628@gmail.com>
2025-10-22 15:22:31 +0530

    Simplify App wrapper to match Home UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,6 @@
 import React from 'react';
 import Home from './pages/Home.jsx';
-import Navbar from './components/Navbar.jsx';
 
 export default function App() {
-  return (
-    <div className="min-h-screen bg-gradient-to-b from-midnight via-twilight to-forest text-mist font-serif">
-      <Navbar />
-      <main className="max-w-[900px] mx-auto my-6 px-4 pb-12">
-        <Home />
-      </main>
-    </div>
-  );
+  return <Home />;
 }
